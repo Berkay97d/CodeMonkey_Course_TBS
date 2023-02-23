25e497af2e83510979e7fe9a24828b4255aa94ce
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-02-23 07:32:46 +0300

    Level Grid

---
+++ Assets/_Scipts/Grid System/LevelGrid.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+public class LevelGrid : MonoBehaviour
+{
+    private GridSystem gridSystem;
+    [SerializeField] private Transform prefab;
+        
+    
+    private void Awake()
+    {
+        gridSystem = new GridSystem(10, 10, 1f);
+        gridSystem.CreateDebugObjects(prefab);
+    }
+
+    public void SetUnitAtGridPosition(GridPosition gridPosition, Unit unit)
+    {
+        
+    }
+
+    public Unit GetUnitAtGridPosition(GridPosition gridPosition)
+    {
+        
+    }
+
+    public void ClearUnitAtGridPosition(GridPosition gridPosition)
+    {
+        
+    }
+}
